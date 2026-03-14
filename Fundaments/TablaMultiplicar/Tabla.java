1a07a3c75658a1e0e48f591c625fb1a7d94e0f8f
Mtrjoel <monterojoel135@gmail.com>
2026-03-14 12:39:59 -0400

    feat: module tabla add

---
+++ Fundaments/TablaMultiplicar/Tabla.java
@@ -0,0 +1,24 @@
+package Fundaments.TablaMultiplicar;
+
+import java.util.Scanner;
+
+public class Tabla {
+    public static void main(String[] args){
+        /**
+         * 1. El usuario elige que tabla desea mostrar
+         * 2. Se muestra la tabla seleccionada desde 1 hasta 12
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        int limiteTabla = 12;
+        System.out.print("Ingrese el numero de la tabla : ");
+        int tabla = scanner.nextInt(); // * entrada de datos
+        for(int i = 1; i <= limiteTabla; i++){
+
+            System.out.format("%d * %d = %d \n",tabla, i, tabla * i);
+        }
+        
+        scanner.close();
+
+    }
+}
