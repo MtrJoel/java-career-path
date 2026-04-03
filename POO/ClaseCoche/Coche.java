8b90bc15facec1c2bdd297e44eba6a14668f5b1c
Mtrjoel <monterojoel135@gmail.com>
2026-04-03 16:13:28 -0400

    feat: implement Coche class with encapsulation, constructor and getters/setters

--- POO/ClaseCoche/Coche.java
+++ POO/ClaseCoche/Coche.java
@@ -1,8 +1,35 @@
 package POO.ClaseCoche;
 
 public class Coche {
-    String marca;
-    String modelo;
+    private String marca;
+    private String modelo;
 
-    MostrarInfo
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setModelo(String modelo){
+        if(modelo != null && !modelo.isEmpty()){
+            this.modelo = modelo;
+        }
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public Coche(String marca, String modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Marca : " + marca);
+        System.out.println("Modelo : " + modelo);
+    }
 }
+
--- POO/ClaseCoche/Main.java
+++ POO/ClaseCoche/Main.java
@@ -2,6 +2,10 @@ package POO.ClaseCoche;
 
 public class Main {
     public static void main(String[] args){
-        
+        Coche c1 = new Coche("Honda", "");
+        Coche c2 = new Coche("Toyota", "");
+
+        System.out.println(c1.getModelo());
+        System.out.println(c2.getModelo());
     }
 }
